2ab92df96bdce14e6c672bd8b1fc2a701205ff47
Ethan Turkeltaub <ethan.turkeltaub@gmail.com>
2014-10-22 13:44:39 -0400

    added canable for user.

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,6 +1,8 @@
 class User
   include Mongoid::Document
   include Mongoid::Timestamps
+  include Canable::Ables
+  include Canable::Cans
 
   field :email, type: String
   field :admin, type: Boolean, default: false
@@ -20,8 +22,13 @@ class User
   field :current_sign_in_ip, type: String
   field :last_sign_in_ip,    type: String
 
-  # field :provider, type: String
-  # field :uid,      type: String
-
   has_many :courses
+
+  def updatable_by?(u)
+    self == u || u.admin?
+  end
+
+  def destroyable_by?(u)
+    updatable_by?(u)
+  end
 end
--- spec/app/models/user_spec.rb
+++ spec/app/models/user_spec.rb
@@ -1,11 +1,25 @@
 require 'spec_helper'
 
 describe User do
-  let(:user) { create :user }
+  let(:user)  { create :user }
+  let(:other) { create :user }
+  let(:admin) { create :user, :admin }
 
   it { expect(user).to be_valid }
 
   it { expect(user).to validate_presence_of :email }
 
   it { expect(user).to validate_uniqueness_of :email }
+
+  describe '#updatable_by?' do
+    it { expect(user.updatable_by?(user)).to eql true }
+    it { expect(user.updatable_by?(other)).to be false }
+    it { expect(user.updatable_by?(admin)).to be true }
+  end
+
+  describe '#destroyable_by?' do
+    it { expect(user.destroyable_by?(user)).to eql true }
+    it { expect(user.destroyable_by?(other)).to be false }
+    it { expect(user.destroyable_by?(admin)).to be true }
+  end
 end
